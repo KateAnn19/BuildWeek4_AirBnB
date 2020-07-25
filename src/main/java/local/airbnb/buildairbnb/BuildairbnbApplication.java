82e9ab81badf9b2075441248992c6e7536bf38a9
Kate McGee <mcgeehee@gmail.com>
2020-07-24 18:35:06 -0600

    initial project setup

---
+++ src/main/java/local/airbnb/buildairbnb/BuildairbnbApplication.java
@@ -0,0 +1,16 @@
+package local.airbnb.buildairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildairbnbApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BuildairbnbApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/local/airbnb/buildairbnb/BuildairbnbApplicationTests.java
@@ -0,0 +1,15 @@
+package local.airbnb.buildairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildairbnbApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
